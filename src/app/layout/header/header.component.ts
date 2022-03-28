0d26a8743651b3478676db67d64ac80080900058
Isaac Gamboa <cigamboar@correo.udistrital.edu.co>
2022-03-28 13:51:32 -0500

    finalizacion de header

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  public isSticky: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    const number = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if (number > 300) {
+      this.isSticky = true;
+    } else {
+      this.isSticky = false;
+    }
+
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { NgbModule, NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 
 
@@ -13,14 +13,16 @@ import { MatSliderModule } from '@angular/material/slider';
     NgbPaginationModule,
     NgbAlertModule,
     MatButtonModule,
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule
   ],
   exports: [
     NgbModule,
     NgbPaginationModule,
     NgbAlertModule,
     MatButtonModule,
-    MatSliderModule
+    MatSliderModule,
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
